89b93f2567613180ef8ede74fb241f0e36f4521c
Ptr <110486254+perekvadrat@users.noreply.github.com>
2023-05-22 01:43:50 +0800

    Add files via upload

---
+++ year.py
@@ -0,0 +1,13 @@
+year = int(input('year '))
+if (year % 4 == 0 and year % 100 != 0):
+    print ('YES')
+elif year % 400 == 0:
+    print ('YES')
+else:
+    print ('NO')
+    
+print("Год високосный" if ((year%4 == 0) and (year%100 != 0)) or (year%400 == 0) else "Год не високосный")
+
+a = 'qwerty'
+for i in a:
+    print (i)
